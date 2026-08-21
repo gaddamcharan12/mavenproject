5b0729bc21c1d2cd3dcaddb25dc02558b8752f8f
Charan <gaddamcharanteja47@gmail.com>
2026-08-21 14:31:28 +0530

    App.java commited

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! CHARAN TEJA@516" );
+        System.out.println( "Hello World! CHARAN TEJA@516 and Sagar" );
     }
 }
